0d5e3843e13967935dc82656dcc8d2aa5971a39c
seung <ksgg1navercom@gmail.com>
2023-05-24 00:39:04 +0900

    converter.py : vsix 자체를 인코딩하도록 변경

--- converter.py
+++ converter.py
@@ -2,45 +2,38 @@ import os
 import json
 import base64
 
-def vsixToText(parentDirectory):
-    json = dict()
-    for dir in os.listdir(parentDirectory):
-        nowDirectory = parentDirectory + "/" + dir
-        if os.path.isfile(nowDirectory) :
-            failed = False
-            with open(nowDirectory, 'r', encoding="utf-8") as f:
-                try :
-                    json[dir] = ''.join(f.readlines())
-                except Exception as e:
-                    failed = True
-            if failed: # 인코딩 ANSI로 시도
-                with open(nowDirectory, 'rb') as f:
-                    print(nowDirectory, "is opened as binary")
-                    try :
-                        binaryData = b''.join(f.readlines())
-                        json[dir] = str(base64.b64encode(binaryData), encoding='UTF-8')
-                    except Exception as e:
-                        json[dir] = ''
-                        print("Error", dir, ":", e)
-        else:
-            json[dir] = vsixToText(nowDirectory)
-    return json
+VSIXDIR = "./vsix"
+TEXTDIR = "./text"
+def vsixToText(fileLocation, fileName):
+    global VSIXDIR
+    global TEXTDIR
 
-def textToVsix(fileName):
-    pass
+    with open(fileLocation, 'rb') as f:
+        binaryData = b''.join(f.readlines())
+        res = str(base64.b64encode(binaryData), encoding='UTF-8')
+        with open(TEXTDIR + "/" + fileName + '.txt', 'w') as g:
+            g.write(res)
 
+def textToVsix(fileLocation, fileName):
+    global VSIXDIR
+    global TEXTDIR
+    with open(fileLocation, 'r') as f:
+        data = ''.join(f.readlines())
+        res = bytes(data, encoding='UTF-8')
+        res = base64.b64decode(res)
+        with open(VSIXDIR + "/" + fileName + '.vsix', 'wb') as g:
+            g.write(res)
 
 def converter():
-    VSIXDIR = "./vsix"
-    TEXTDIR = "./text"
-
-    result = dict()
     for dir in os.listdir(VSIXDIR):
-        if not os.path.isfile(dir) :
-            print("convert vsix to json :", dir)
-            result[dir] =  vsixToText(VSIXDIR + "/" + dir)
-        with open('./text/' + dir +'.json', 'w') as fp:
-            json.dump(result, fp)
+        if os.path.isfile(dir) :
+            print("convert vsix to text :", dir)
+            vsixToText(VSIXDIR + "/" + dir)
+    
+    for dir in os.listdir(TEXTDIR):
+        # if os.path.isfile(dir) :
+            print("convert text to visx :", dir)
+            textToVsix(TEXTDIR + "/" + dir)
 
 if __name__ == '__main__':
     converter()
