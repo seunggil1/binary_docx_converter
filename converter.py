fd33cc3403415c39112561d0b47fcaa7d4765e22
seung <ksgg1navercom@gmail.com>
2023-05-26 16:53:58 +0900

    word기반으로 수정

--- converter.py
+++ converter.py
@@ -1,34 +1,50 @@
 import os
 import json
 import base64
-import pdfkit
+from docx import Document
 # import pdftotext
 
 VSIXDIR = "./vsix"
 TEXTDIR = "./text"
-# config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
 def vsixToText(fileLocation, fileName):
     global VSIXDIR
     global TEXTDIR
-    global config
+
     with open(fileLocation, 'rb') as f:
         binaryData = b''.join(f.readlines())
         res = base64.b64encode(binaryData)
         res = res.decode('utf-8')
-        with open(TEXTDIR + "/" + fileName + '.txt', 'w', encoding='utf-8') as g:
-            g.write(res)
+        doc = Document()
+        doc.add_paragraph(res)
+        doc.save(TEXTDIR + "/" + fileName + '.docx')
+    # with open(fileLocation, 'rb') as f:
+    #     binaryData = b''.join(f.readlines())
+    #     res = base64.b64encode(binaryData)
+    #     res = res.decode('utf-8')
+    #     with open(TEXTDIR + "/" + fileName + '.txt', 'w', encoding='utf-8') as g:
+    #         g.write(res)
 def textToVsix(fileLocation, fileName):
     global VSIXDIR
     global TEXTDIR
-    with open(fileLocation, 'r', encoding='utf-8') as f:
-        data = ''.join(f.readlines())
-        res = bytes(data, encoding='utf-8')
-        res = base64.b64decode(res)
-        with open(VSIXDIR + "/" + fileName + '.vsix', 'wb') as g:
+
+    doc = Document(fileLocation)
+    data = doc.paragraphs[0].text
+    data = data.encode(encoding='utf-8')
+    res = base64.b64decode(data)
+    with open(VSIXDIR + "/" + fileName + '.vsix', 'wb') as g:
             g.write(res)
 
+    # with open(fileLocation, 'r', encoding='utf-8') as f:
+    #     data = ''.join(f.readlines())
+    #     res = bytes(data, encoding='utf-8')
+    #     res = base64.b64decode(res)
+    #     with open(VSIXDIR + "/" + fileName + '.vsix', 'wb') as g:
+    #         g.write(res)
+
 def converter():
-    for dir in os.listdir(VSIXDIR):
+    r = os.listdir(VSIXDIR)
+    r.reverse()
+    for dir in r:
         # if os.path.isfile(dir) :
             print("convert vsix to text :", dir)
             vsixToText(VSIXDIR + "/" + dir, dir)
